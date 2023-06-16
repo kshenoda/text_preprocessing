412aa4ba703925bde5221f39c48673d7bb0f0773
kshenoda <133154644+kshenoda@users.noreply.github.com>
2023-06-16 20:07:06 +0300

    note

--- app.py
+++ app.py
@@ -19,7 +19,8 @@ def text_preprocessing():
 
     tokens = word_tokenize(text)
 
-    stop_words = set(stopwords.words('english'))
+    stop_words = set(["mightn't", 're','wasn', 'wouldn', 'be', 'has', 'that', 'does', 'shouldn', 'do', "you've",'off', 'for', "didn't", 'm', 'ain', 'haven', "weren't", 'are', "she's", "wasn't", 'its', "haven't", "wouldn't", 'don', 'weren', 's', "you'd", "don't", 'doesn', "hadn't", 'is', 'was', "that'll", "should've", 'a', 'then', 'the', 'mustn', 'i', 'nor', 'as', "it's", "needn't", 'd', 'am', 'have',  'hasn', 'o', "aren't", "you'll", "couldn't", "you're", "mustn't", 'didn', "doesn't", 'll', 'an', 'hadn', 'whom', 'y', "hasn't", 'itself', 'couldn', 'needn', "shan't", 'isn', 'been', 'such', 'shan', "shouldn't", 'aren', 'being', 'were', 'did', 'ma', 't', 'having', 'mightn', 've', "isn't", "won't"])
+
     filtered_tokens = [word for word in tokens if word.lower() not in stop_words]
 
     tagged = nltk.pos_tag(tokens)
